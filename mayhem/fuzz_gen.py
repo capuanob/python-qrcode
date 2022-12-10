ae5b30bd9aabc6603860eb178900a424d26bf01e
bcapuano <bcapuano@asu.edu>
2022-12-10 13:18:08 -0500

    Less coverage

--- mayhem/fuzz_gen.py
+++ mayhem/fuzz_gen.py
@@ -1,24 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import atheris
 import logging
 import sys
 
 with atheris.instrument_imports(include=[
-    'qrcode.constants',
-    'qrcode.exceptions',
-    'qrcode.util',
-    'qrcode.LUT',
-    'qrcode.base',
-    'qrcode.image',
-    'qrcode.image-base',
-    'qrcode.image.styles',
-    'qrcode.image.styles.moduledrawers',
-    'qrcode.image.styles.moduledrawers.pil',
-    'qrcode.compat',
-    'qrcode.compat.util',
-    'qrcode.image.styles.moduledrawers.base',
-    'qrcode.image.pure'
-    ]):
+    'qrcode'
+    #'qrcode.constants',
+    #'qrcode.exceptions',
+    #'qrcode.util',
+    #'qrcode.LUT',
+    #'qrcode.base',
+    #'qrcode.image',
+    #'qrcode.image-base',
+    #'qrcode.image.styles',
+    #'qrcode.image.styles.moduledrawers',
+    #'qrcode.image.styles.moduledrawers.pil',
+    #'qrcode.compat',
+    #'qrcode.compat.util',
+    #'qrcode.image.styles.moduledrawers.base',
+    #'qrcode.image.pure'
+   ],
+   exclude=[
+       'qrcode.compat.pil',
+       'qrcode.image.styles.moduledrawers.pil',
+       'qrcode.constants',
+       'qrcode.exceptions',
+       'qrcode.util',
+       'qrcode.LUT',
+       'qrcode.image',
+       'qrcode.image-base',
+       'qrcode.image.styles',
+       'qrcode.image.styles.moduledrawers',
+       'qrcode.image.styles.moduledrawers.pil',
+       'qrcode.compat',
+       'qrcode.compat.util',
+       'qrcode.image.styles.moduledrawers.base',
+       'qrcode.image.pure'
+       ]):
     import qrcode
 
 # No logging
@@ -30,10 +48,12 @@ error_correction_opts = [qrcode.constants.ERROR_CORRECT_L,
                          qrcode.constants.ERROR_CORRECT_H]
 
 
-@atheris.instrument_func
 def TestOneInput(data):
     try:
-        img = qrcode.make(data)
+        # img = qrcode.make(data)
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_Q, box_size=1, border=0)
+        qr.add_data(data)
+        qr.make(fit=True)
     except ValueError as e:
         if 'glog' in str(e):
             return -1
